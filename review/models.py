f35397969a58cd74f8525114c8ab6b60170c4bc3
Miguel <maarcilac@eafit.edu.co>
2024-08-12 22:41:13 -0500

    Implementar funcionalidad para agregar comentarios de profesores

--- review/models.py
+++ review/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from profesores.models import Profesor
 
-# Create your models here.
+class Comentario(models.Model):
+    profesor = models.ForeignKey(Profesor, related_name='comentarios', on_delete=models.CASCADE)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    contenido = models.TextField()
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comentario de {self.usuario} sobre {self.profesor}'
--- review/views.py
+++ review/views.py
@@ -1,6 +1,41 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Comentario, Profesor
+from .forms import ComentarioForm
+from profesores.models import Profesor
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+
+
+
+@login_required
+def agregar_comentario(request, profesor_id):
+    profesor = get_object_or_404(Profesor, pk=profesor_id)
+    if request.method == 'POST':
+        form = ComentarioForm(request.POST)
+        if form.is_valid():
+            comentario = form.save(commit=False)
+            comentario.profesor = profesor
+            comentario.usuario = request.user
+            comentario.save()
+            return redirect('detalle_profesor', profesor_id=profesor.id)
+    else:
+        form = ComentarioForm()
+    return render(request, 'review/agregar_comentario.html', {
+        'form': form,
+        'profesor': profesor
+    })
+def detalle_profesor(request, profesor_id):
+    profesor = get_object_or_404(Profesor, pk=profesor_id)
+    comentarios = profesor.comentarios.all()
+    return render(request, 'profesores/detalle_profesor.html', {
+        'profesor': profesor,
+        'comentarios': comentarios
+    })
+
+@login_required
+def user_profile(request):
+    return render(request, 'review/user_profile.html', {'user': request.user})
